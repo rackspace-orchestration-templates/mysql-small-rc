992b3bdb072551994fb76f6dde0398acbec7a4af
Chris Hultin <chris.hultin@rackspace.com>
2016-05-24 22:51:50 -0500

    Fixing test

--- test/fabric/mysql.py
+++ test/fabric/mysql.py
@@ -10,11 +10,11 @@ def check():
   assert port.is_listening(3306), 'port 3306/mysql is not listening'
 
   if (env.platform_family == "rhel"):
-    assert process.is_up('httpd'), 'apache is not running'
-    assert service.is_enabled('httpd'), 'apache is not enabled'
-  elif (env.platform_family == 'debian'): 
+    assert process.is_up('mysqld'), 'apache is not running'
+    assert service.is_enabled('mysqld'), 'apache is not enabled'
+  elif (env.platform_family == 'debian'):
     assert process.is_up('mysql'), ' is not running'
-    assert service.is_enabled('mysql'), 'mysql is not enabled' 
+    assert service.is_enabled('mysql'), 'mysql is not enabled'
 
 @task
 def artifacts():
